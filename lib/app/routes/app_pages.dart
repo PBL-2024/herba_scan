1c999c46af0495f8f233d7c34f526142513971f1
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-10-23 11:06:44 +0700

    Login and Signup page

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,5 +1,4 @@
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:herba_scan/app/modules/auth/views/forget_password_view.dart';
 import '../modules/auth/bindings/auth_binding.dart';
 import '../modules/auth/views/auth_view.dart';
